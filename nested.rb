e87eaefc90624da39b18332f5f704fd34440072c
confident-token-1019 <theoonetj@gmail.com>
2019-12-20 20:29:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -26,15 +26,7 @@ def alan_kay_is_known_for
  		{
         :grace_hopper => {
           :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["COBOL" "FORTRAN"]
         }
      }
 end
